144d2ad760945898fbdb2d1d8392ba1e69da50a1
Alexander <1735195@gmail.com>
2022-03-14 22:10:04 +0300

    chore: add assets

--- apps/clipz/src/app/app.component.ts
+++ apps/clipz/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'clipz-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'clipz';
